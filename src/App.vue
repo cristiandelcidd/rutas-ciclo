d948aa39be106d936815f23bcaa27961404e03e6
cristiandelcidd <cristian_delcid10@yahoo.com>
2022-10-26 23:08:58 -0600

    router-link added

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,30 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Navbar />
+  <router-view></router-view>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { defineAsyncComponent } from "vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Navbar: defineAsyncComponent(() =>
+      import(
+        /* webpackChunkName: "Navbar" */ "@/modules/shared/components/Navbar"
+      )
+    ),
+  },
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  -webkit-font-smoothing: antialiased;
   color: #2c3e50;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
   margin-top: 60px;
+  text-align: center;
 }
 </style>
---
+++ src/modules/shared/components/Navbar.vue
@@ -0,0 +1,26 @@
+<template>
+  <nav>
+    <router-link to="/">Pokemon List</router-link>
+    <router-link to="/id">Pokemon by id</router-link>
+    <router-link to="/about">About</router-link>
+  </nav>
+</template>
+
+<style scoped>
+nav {
+  display: flex;
+  justify-content: center;
+  gap: 1rem;
+}
+
+a,
+a:active {
+  color: #000;
+  text-decoration: none;
+}
+
+.router-link-exact-active {
+  color: #42b983;
+  text-decoration: underline;
+}
+</style>
